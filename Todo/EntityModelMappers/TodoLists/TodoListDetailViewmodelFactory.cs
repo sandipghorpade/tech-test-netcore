d74c2701ae382dc16a6400f0fde2516d0633f118
sandip.ghorpade0808@gmail.com <sandip.ghorpade0808@gmail.com>
2024-05-22 12:55:42 +0530

    Task 2- Added code to change the display order of Items in detail page by order of importance: High, Medium, Low

--- Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
+++ Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Todo.Data.Entities;
 using Todo.EntityModelMappers.TodoItems;
+using Todo.Models.TodoItems;
 using Todo.Models.TodoLists;
 
 namespace Todo.EntityModelMappers.TodoLists
@@ -9,8 +11,15 @@ namespace Todo.EntityModelMappers.TodoLists
     {
         public static TodoListDetailViewmodel Create(TodoList todoList)
         {
-            var items = todoList.Items.Select(TodoItemSummaryViewmodelFactory.Create).ToList();
+            var items = todoList.Items
+                                     .Select(TodoItemSummaryViewmodelFactory.Create)
+                                     .OrderByImportance();
             return new TodoListDetailViewmodel(todoList.TodoListId, todoList.Title, items);
+        }
+
+        public static IList<TodoItemSummaryViewmodel> OrderByImportance(this IEnumerable<TodoItemSummaryViewmodel> list)
+        {
+            return list.OrderBy(li => li.Importance).ToList();
         }
     }
 }
