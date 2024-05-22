dbda02187ad8945bfa3bb391d6b5e9973409975f
sandip.ghorpade0808@gmail.com <sandip.ghorpade0808@gmail.com>
2024-05-22 15:07:34 +0530

    Task 5- Added code to hide & show items that are marked as done on details page.

---
+++ Todo/wwwroot/js/todolistdetails.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $("#toggleCompletedItems").click(function () {
+        var link = $(this).find("strong");
+        var isHideCompleted = link.text() === "Hide Completed Items";
+
+        $(".list-group-item").each(function () {
+            var itemTitle = $(this).find('input[name="item.IsDone"]');
+            if (itemTitle.length>0) {
+                $(this).toggle(!isHideCompleted);
+            }
+        });
+
+        link.text(isHideCompleted ? "Show Completed Items" : "Hide Completed Items");
+    });
+});
